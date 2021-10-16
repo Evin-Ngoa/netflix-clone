1eca805aae28d9134f263f2d439606c04f37aa20
Evingtone Mwailongo <toneevin07@gmail.com>
2021-10-17 00:20:35 +0300

    Sign In Page Static

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,9 +1,40 @@
 import React from "react";
+import { HeaderContainer } from '../containers/header';
+import { FooterContainer } from '../containers/footer';
+import { Form } from '../components';
 
 export default function SignIn() {
     return (
-        <p>
-            Hello From SignIn.
-        </p>
+        <>
+            <HeaderContainer>
+                <Form>
+                    <Form.Title>Sign In</Form.Title>
+
+                    <Form.Base  method="POST">
+                    <Form.Input
+                        placeholder="Email address"
+                        value=""
+                    />
+                    <Form.Input
+                        type="password"
+                        value=""
+                        autoComplete="off"
+                        placeholder="Password"
+                    />
+                    <Form.Submit disabled="" type="submit" data-testid="sign-in">
+                        Sign In
+                    </Form.Submit>
+                    </Form.Base>
+
+                    <Form.Text>
+                        New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
+                    </Form.Text>
+                    <Form.TextSmall>
+                        This page is protected by Google reCAPTCHA to ensure you're not a bot. Learn more.
+                    </Form.TextSmall>
+                </Form>
+            </HeaderContainer>
+            <FooterContainer/>
+        </>
     );
 }
