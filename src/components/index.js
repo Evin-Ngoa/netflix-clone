4c1e36455c7cd406576a2677e06e7533f0bc3597
Evingtone Mwailongo <toneevin07@gmail.com>
2021-10-16 18:00:13 +0300

    Footer Component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 
 // Register all component exports here so that the can be imported like so `import { Jumbotron } from '../components/';` in containers/jumbotron.js
 export { default as Jumbotron } from './jumbotron';
+export { default as Footer } from './footer';
