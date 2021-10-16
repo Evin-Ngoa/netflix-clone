1bf75f4d725db5783cb0ab03d9aca23291729451
Evingtone Mwailongo <toneevin07@gmail.com>
2021-10-16 17:15:16 +0300

    Refactored app.js to use containers

---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+
+// Register all component exports so that the can be imported like so `import { JumbotronContainer } from './containers/jumbotron';` in App.js
+export { default as Jumbotron } from './jumbotron';
