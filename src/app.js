d9e609bd30f23ffdc4b9264ef60b5dce50df9b18
Evingtone Mwailongo <toneevin07@gmail.com>
2021-10-17 21:49:57 +0300

    Check url route with switch statement

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import { Home, SignIn, SignUp, Browse }  from './pages';
 import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
 import { useAuthListener } from './hooks';
@@ -10,29 +10,21 @@ export default function App() {
    const user = useAuthListener();
 
     return (
-        <Router>
-             <Route exact path={ROUTES.HOME}>
-
-                <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME}>
-                  <Home/>
-               </IsUserRedirect>
-             </Route>
-             <Route exact path={ROUTES.SIGN_IN}>
-                {/* Check if use is loggedin */}
+         <Router>
+            <Switch>
                <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN}>
                   <SignIn />
                </IsUserRedirect>
-             </Route>
-             <Route exact path={ROUTES.SIGN_UP}>
-                <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP}>
-                  <SignUp/>
+               <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP}>
+                  <SignUp />
                </IsUserRedirect>
-             </Route>
-             <Route exact path={ROUTES.BROWSE}>
-                <ProtectedRoute user={user} path={ROUTES.BROWSE}>
+               <ProtectedRoute user={user} path={ROUTES.BROWSE}>
                   <Browse />
                </ProtectedRoute>
-             </Route>
-        </Router>
+               <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME}>
+                  <Home />
+               </IsUserRedirect>
+            </Switch>
+         </Router>
     );
 }
