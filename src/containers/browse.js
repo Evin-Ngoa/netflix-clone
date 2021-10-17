be6c62401876f243f663b0e42d72f52dd5913b38
Evingtone Mwailongo <toneevin07@gmail.com>
2021-10-17 19:24:27 +0300

    Add Spinner Loader to profile

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { SelectProfileContainer } from "../containers/profile";
 import { FirebaseContext } from "../context/firebase";
+import { Loading } from "../components";
 
 export function BrowseContainer({ slides }) {
     const [profile, setProfile] = useState({});
@@ -15,5 +16,10 @@ export function BrowseContainer({ slides }) {
         }, 3000);
     }, [profile.displayName]);
 
-    return <SelectProfileContainer user={user} setProfile={setProfile} />
+    
+    return profile.displayName ? (
+        <> 
+            {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
+        </>
+        ) : <SelectProfileContainer user={user} setProfile={setProfile} /> ;
 }
