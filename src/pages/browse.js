3ed458a95f8cf6ee85e4bdf698ddddbb5cc9832e
Evingtone Mwailongo <toneevin07@gmail.com>
2021-10-17 18:16:19 +0300

    Create Hook for reading series/films from firebase in browse page

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,6 +1,14 @@
 import React from "react";
+import { useContent } from '../hooks';
 
 export default function Browse() {
+    // Need series and the films
+    const { series } = useContent('series');
+    const { films } = useContent('films');
+    
+    // Need slides
+    // pass to the browse container
+
     return (
         <p>
             Hello From Browse.
