7db1f0c7d0dfddf3c0037fe9209562a3bde5bb08
Evingtone Mwailongo <toneevin07@gmail.com>
2021-10-17 18:29:05 +0300

    Browse Container

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,17 +1,16 @@
 import React from "react";
 import { useContent } from '../hooks';
+import { selectionFilter } from '../utils';
+import { BrowseContainer } from '../containers/browse';
 
 export default function Browse() {
-    // Need series and the films
+    // Implement series and the films
     const { series } = useContent('series');
     const { films } = useContent('films');
-    
-    // Need slides
-    // pass to the browse container
 
-    return (
-        <p>
-            Hello From Browse.
-        </p>
-    );
+    // Implement slides
+    const slides = selectionFilter({ series, films });
+
+    // pass to the browse container
+    return <BrowseContainer slides={slides} />
 }
